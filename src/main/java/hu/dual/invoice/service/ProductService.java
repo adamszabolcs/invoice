c1173ab053a9dac443189d1500b4872efc697ae0
adamszabolcs <szabolcs.adam91@gmail.com>
2021-08-23 21:13:35 +0200

    deleted @Component from ProductService

--- src/main/java/hu/dual/invoice/service/ProductService.java
+++ src/main/java/hu/dual/invoice/service/ProductService.java
@@ -7,7 +7,6 @@ import hu.dual.invoice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class ProductService {
 
     @Autowired
