1ad4f67b91754cf397466086f3a9bc0ea8e6fad3
adamszabolcs <szabolcs.adam91@gmail.com>
2021-08-25 21:15:06 +0200

    add new route to exchangeRate, calculate EUR in get requests

--- src/main/java/hu/dual/invoice/controller/WebController.java
+++ src/main/java/hu/dual/invoice/controller/WebController.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.dual.invoice.model.Invoice;
 import hu.dual.invoice.model.InvoiceItem;
+import hu.dual.invoice.service.CurrencyExchangeService;
 import hu.dual.invoice.service.InvoiceItemService;
 import hu.dual.invoice.service.InvoiceService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -24,16 +25,23 @@ public class WebController {
     @Autowired
     InvoiceItemService invoiceItemService;
 
+    @GetMapping("/get-exchange-rate")
+    public BigDecimal getExchangeRate() {
+        return CurrencyExchangeService.exchangeRate;
+    }
+
+
     @GetMapping("/invoices")
     public List<Invoice> getInvoices() {
         List<Invoice> invoiceList = invoiceService.getAllInvoice();
+        invoiceList = invoiceService.calculateTotalPriceInEUR(invoiceList);
         return invoiceList;
     }
 
     @GetMapping("/invoice/{invoice-id}")
     public List<InvoiceItem> getInvoiceItems(@PathVariable("invoice-id") String invoiceId) {
-        Invoice invoice = invoiceService.getInvoiceById(invoiceId);
-        List<InvoiceItem> invoiceItems = invoiceItemService.getAllInvoiceItemByInvoice(invoice);
+        List<InvoiceItem> invoiceItems = invoiceItemService.getAllInvoiceItemByInvoiceId(invoiceId);
+        invoiceItems = invoiceItemService.calculateTotalPriceInEUR(invoiceItems);
         return invoiceItems;
     }
 
