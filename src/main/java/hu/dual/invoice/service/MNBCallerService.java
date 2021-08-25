b88f5dea12981ce51bbff83efb1f0dec88e6ec6c
adamszabolcs <szabolcs.adam91@gmail.com>
2021-08-25 21:19:32 +0200

    implemented getActualExchangeRate webservice call

--- src/main/java/hu/dual/invoice/service/MNBCallerService.java
+++ src/main/java/hu/dual/invoice/service/MNBCallerService.java
@@ -1,24 +1,39 @@
 package hu.dual.invoice.service;
 
 import hu.dual.invoice.model.Currencies;
-import hu.mnb.webservices.GetExchangeRatesRequestBody;
+import hu.dual.invoice.util.XMLUtil;
+import hu.mnb.webservices.*;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
-public class MNBCaller {
+public class MNBCallerService {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    public BigDecimal getActualExchangeRate(Currencies currency) {
+    public BigDecimal getActualExchangeRate(Currencies currency) throws MNBArfolyamServiceSoapGetExchangeRatesStringFaultFaultMessage {
+
+        MNBArfolyamServiceSoapImpl service = new MNBArfolyamServiceSoapImpl();
+        MNBArfolyamServiceSoap client = service.getCustomBindingMNBArfolyamServiceSoap();
+
         GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
         request.setCurrencyNames(currency.name());
         request.setStartDate(dateFormat.format(new Date()));
         request.setEndDate(dateFormat.format(new Date()));
-        return null;
+
+        GetExchangeRatesResponseBody response = client.getExchangeRates(request);
+        System.out.println(response.getGetExchangeRatesResult());
+        Document doc = XMLUtil.stringToDocument(response.getGetExchangeRatesResult());
+
+        Node rateNode = doc.getFirstChild().getFirstChild();
+        String rateString = rateNode.getTextContent();
+        return BigDecimal.valueOf(Double.parseDouble(rateString.replace(",", ".")));
+
     }
 
 }
