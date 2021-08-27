2a91e1ab19932e9a22f311e80eb68a8a2a6845ad
adamszabolcs <szabolcs.adam91@gmail.com>
2021-08-27 14:17:15 +0200

    add Repository annotation

--- src/main/java/hu/dual/invoice/repository/InvoiceItemRepository.java
+++ src/main/java/hu/dual/invoice/repository/InvoiceItemRepository.java
@@ -1,14 +1,14 @@
 package hu.dual.invoice.repository;
 
-import hu.dual.invoice.model.Invoice;
 import hu.dual.invoice.model.InvoiceItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, String> {
 
-    List<InvoiceItem> findAllByInvoice(Invoice invoice);
     List<InvoiceItem> findAllByInvoiceId(String invoiceId);
 
 }
